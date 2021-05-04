6ad2e384a24b3a64b0ebc2fa66bcd2e21c68c832
Stackbit Code Editor <projects@stackbit.com>
2021-05-04 16:05:18 +0000

    about-page.js: changed

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import SimpleSlider from "../components/slideshow"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
